df903d79191698ef9ae1678b008df9c182701765
Scott Moore (Scott Moore Software) <scott.moore@scottmooresoftware.com>
2019-06-22 10:49:32 +0100

    Add files for typescript

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import "Phaser"
+import config from './config/config';
+import GameScene from './scenes/gamescene';
+
+class HauntletGame extends Phaser.Game {
+  constructor(config: Phaser.Types.Core.GameConfig) {
+    super(config);
+
+    console.log('HauntletGame::constructor()');
+
+    this.scene.add('GameScene', GameScene);
+    this.scene.start('GameScene');
+  }
+}
+
+window.onload = function () {
+  //window.game = new HauntletGame();
+
+  var game = new HauntletGame(config);
+}
